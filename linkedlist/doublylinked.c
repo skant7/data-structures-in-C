1b52b95d46b05b806bc0525fc1ad1aa39a276391
Surya Kant <suryakantasahoo7351@gmail.com>
2020-09-28 06:31:25 +0530

    Doubly Linked List

--- linkedlist/doublylinked.c
+++ linkedlist/doublylinked.c
@@ -42,7 +42,7 @@ int main(){
                     start = addtoempty(start,data);
                     break;
                 case 4:
-                    printf("Enter data to be entered: ");
+                    printf("Enter data to be inserted: ");
                     scanf("%d",&data);
                     start = addatbeg(start,data);
                     break;
@@ -50,6 +50,31 @@ int main(){
                     printf("Enter data to be inserted: ");
                     scanf("%d",&data);
                     start = addatend(start,data);
+                    break;
+                case 6:
+                    printf("Enter data to be inserted: ");
+                    scanf("%d",&data);
+                    printf("Enter item after which to insert: ");
+                    scanf("%d",&item);
+                    start = addafter(start,data,item);
+                    break;
+                case 7:
+                    printf("Enter data to be inserted: ");
+                    scanf("%d",&data);
+                    printf("Enter item before which to insert: ");
+                    scanf("%d",&item);
+                    start = addbefore(start,data,item);
+                    break;
+                case 8:
+                    printf("Enter data to be deleted: ");
+                    scanf("%d",&item);
+                    start = del(start,item);
+                    break;
+                case 9:
+                    start = reverse(start);
+                    break;
+                case 10:
+                    exit(1);
                     break;
                 default:
                     exit(1);
@@ -59,6 +84,108 @@ int main(){
         }
    return 0;          
 }
+struct node *reverse(struct node *start){
+    struct node *p1,*p2;
+    p1 = start;
+    p2 = p1->next;
+    p1->next = NULL;
+    p1->prev = p2;
+    while(p2!=NULL){
+        p2->prev = p2->next;
+        p2->next = p1;
+        p1 = p2;
+        p2 = p2->prev;
+    }
+ start = p1;
+ return start;
+}
+struct node *del(struct node *start ,int item){
+    struct node *tmp;
+    if(start == NULL){
+        printf("Empty List!\n");
+        return start;
+    }
+    if(start ->next == NULL){               //Deletion of only node
+            tmp = start;
+            start = NULL;
+            free(tmp);
+            return start;
+    }
+    if(start->info == item){                //Deletion of first element
+         tmp = start;               
+         start = start ->next;
+         start->next->prev = NULL;
+         free(tmp);
+         return start;
+    }
+    tmp = start;
+    while(tmp->next != NULL){               //Deletion in between
+        if(tmp->info == item){
+            tmp->prev->next = tmp->next;
+            tmp->next->prev = tmp->prev;
+            free(tmp);
+            return start;
+        }
+        tmp = tmp->next;
+    }
+    if(tmp->info == item){                  //Deletion at last
+        tmp->prev->next = NULL;
+        free(tmp);
+        return start;
+    }
+  printf("Element not found!\n");
+  return start;
+}
+struct node *addbefore(struct node *start,int data,int item){
+    struct node *tmp,*q;
+    tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->info = data;
+    if(start == NULL){
+        printf("List is empty!\n");
+        return start;
+    }
+    if(start->info ==item){
+        tmp->next = start;
+        tmp->prev=NULL;
+        start->prev=tmp;
+        start=tmp;
+        return start;
+    }
+    q = start;
+    while(q!=NULL){
+        if(q->info == item){
+            tmp->next=q;
+            tmp->prev= q->prev;
+            q->prev->next = tmp;
+            q->prev = tmp;
+            return start;
+        }
+        q= q->next;
+    }
+ printf("Element not found!\n");
+ return start;
+}
+struct node *addafter(struct node *start,int data,int item){
+  struct node *tmp,*p;
+  tmp = (struct node *)malloc(sizeof(struct node));
+  tmp ->info= data;
+  p= start;
+  while(p->next!=NULL){
+      if(p->info == item){
+           tmp->prev = p;
+           tmp->next=p->next;
+           if(p->next!=NULL)
+                p->next->prev=tmp;
+           p->next = tmp;
+           return start;
+      }
+    p= p->next;
+  }
+ printf("Element not found!\n");
+ return start;
+}
+
+
 void display(struct node *start){
     struct node *p;
     if(start == NULL){
